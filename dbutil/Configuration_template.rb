e04b078b5af781abbcae177cafd820573c340738
bbearce <bbearce@gmail.com>
2021-08-19 18:47:13 -0400

    adding some DICOM processing scripts

--- dbutil/Configuration_template.rb
+++ dbutil/Configuration_template.rb
@@ -1,9 +1,10 @@
- module Configuration
+module Configuration
   DNS = 'localhost'
   IMAGES_DB = '<db_name>'
   DB_PORT = '5984'
   HTTP_PORT = '8080'
   DB_ADMIN_USER = '<admin>'
   DB_ADMIN_PASS = '<password>'
+  ADMIN_PARTY = false
 end
 
